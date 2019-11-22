a019f0764bf70d39632c4b27f9ae4bbccdaf3e15
Sergey Leonov <brottys@gmail.com>
2019-11-22 16:56:26 +0300

    Implemented real parsing

--- FDoctor/CoreLayer/Networking/Parsers/PillsParser.swift
+++ FDoctor/CoreLayer/Networking/Parsers/PillsParser.swift
@@ -9,15 +9,45 @@
 import Foundation
 
 struct PillApiModel {
+    let id: Int
     let name: String
+    let img: String
+    let description: String
+    let dose: String
 }
 
 class PillsParser: IParser {
     
     typealias Model = [PillApiModel]
     
-    func parse(data: Data) -> [PillApiModel]? {
-        return [PillApiModel(name: "Мезим форте")]
+    func parse(data: Data) -> [PillApiModel]? {        
+        do {
+            guard let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: AnyObject] else {
+                return nil
+            }
+            
+            guard let pills = json["pills"] as? [[String: Any]] else {
+                return nil
+            }
+            
+            var pillApiModels: [PillApiModel] = []
+            
+            for item in pills {
+                guard let id = item["id"] as? Int,
+                    let name = item["name"] as? String,
+                    let img = item["img"] as? String,
+                    let description = item["desription"] as? String,
+                    let dose = item["dose"] as? String else {
+                        continue
+                }
+                pillApiModels.append(PillApiModel(id: id, name: name, img: img, description: description, dose: dose))
+            }
+            
+            return pillApiModels
+        } catch {
+            print(error.localizedDescription)
+            return nil
+        }
     }
     
 }
